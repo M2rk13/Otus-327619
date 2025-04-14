a7313741bfc2f6f1b6ddf152265be918ef2a9964
M2rk13 <morgunov.mark@bk.ru>
2025-04-14 10:42:21 +0300

    task_3-fixes

--- chessboard/main.go
+++ chessboard/main.go
@@ -1,38 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var size = 8
+	var size int
+	const defaultValue = 8
 
-	chessBoard := generateChessboard(size)
-	fmt.Print(chessBoard)
+	fmt.Printf("Enter chessboard size (default %d): ", defaultValue)
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	if input == "" {
+		size = defaultValue
+	} else {
+		size, _ = strconv.Atoi(input)
+	}
+
+	fmt.Print(generateChessboard(size))
 }
 
 func generateChessboard(size int) string {
-	var chess string
-	var cell string
+	var chessBoard strings.Builder
 
 	const black string = "#"
 	const white string = "_"
 	const separator string = "|"
 
 	for x := 1; x <= size; x++ {
-		chess = chess + separator
+		chessBoard.WriteString(separator)
 
 		for y := 1; y <= size; y++ {
 			switch (x + y) % 2 {
 			case 0:
-				cell = black
+				chessBoard.WriteString(black)
 			case 1:
-				cell = white
+				chessBoard.WriteString(white)
 			}
 
-			chess = chess + cell + separator
+			chessBoard.WriteString(separator)
 		}
 
-		chess = chess + "\n"
+		chessBoard.WriteString("\n")
 	}
 
-	return chess + separator
+	chessBoard.WriteString(separator)
+
+	return chessBoard.String()
 }
